ea8157ecea6572674a343a7e6fcc46c11e863d0a
andsainz <dreasvi@gmail.com>
2023-10-02 11:08:54 +0200

    update gitignore and brands crud done

--- controllers/brands-controller.js
+++ controllers/brands-controller.js
@@ -1,13 +1,13 @@
-import BrandModel from "../models/brand-model.js"
+import BrandModel from "../models/brand-model";
 
 export class BrandController {
     static async getAllBrands(req, res) {
-        const brands = await BrandModel.getAll
-        res.json(brands)
+        const brand = await BrandModel.findAll()
+        res.json(brand)
 }
     static async getBrandById (req, res){
-            const { id } = req.params
-            const brand = await BrandModel.getBrandById({id})
+            const id = req.params.id;
+            const brand = await BrandModel.findByPk(id)
             if(brand) return res.json(brand)
             res.status(404).json({message: 'brand not found'})
     }
@@ -17,27 +17,25 @@ export class BrandController {
         if(result.error){
             return res.status(400).json({error: JSON.parse(result.error.message)})
         }
-        const newBrand = await BrandModel.createBrand({input: result.data})
+        const newBrand = await BrandModel.create(result)
         res.status(201).json(newBrand)
     }
 
     static async deleteBrand(req, res) {
-        const { id } = req.params
-        const result = await BrandModel.deleteBrand({id})
+        const id = req.params.id
+        const result = await BrandModel.destroy({where: {id:req.params.id}})
         if(result === false) return res.status(404).json({ message: 'brand not found' })
         return res.json({ message: 'brand deleted' })
     }
     
     static async updateBrand(req, res) {
     
-        const result = req.body
+        const result =req.body
     
         if(result.error){
             return res.status(400).json({error: JSON.parse(result.error.message)})
         }
-    
-        const { id } = req.params
-        const updatedBrand = await BrandModel.updateBrand({id, input: result.data})
-        return res.json(updatedBrand)
+        const updatedBrand = await BrandModel.update(result, {where: {id: req.params.id}})
+        return res.json({message: 'your brand has been updated suscesfully!'})
     }
 }
